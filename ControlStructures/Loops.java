cb942a5120aacc9334833cf6d98e9ee9e2c6b2e6
cheinks <cheinks1074@gmail.com>
2018-08-28 12:39:58 -0700

    added a method

--- ControlStructures/Loops.java
+++ ControlStructures/Loops.java
@@ -24,6 +24,7 @@ public class Loops
             System.out.println(i);
         }
     }
+    
     public static void litLoop(){
         for(int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
@@ -34,8 +35,27 @@ public class Loops
     }
     public static void litLoop2(){
         for(int i = 1; i < 6; i++){
-            for (int j = i; j < 6; j++){
+            for(int j = i; j < 6; j++){
                 System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void challenge1(){
+        for(int i = 1; i < 6; i++){
+            for(int j = i; j < 2*i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void challenge2(){
+        for(int i = 5; i > 0; i--){
+            int num = i;
+            for(int j = i; j > 0; j--){
+                System.out.print(num + " ");
+                num *= i;
             }
             System.out.println();
         }
