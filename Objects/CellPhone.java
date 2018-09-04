6c079985d5ca7a1762e16a9237c7f47142a37c25
cheinks <cheinks1074@gmail.com>
2018-09-04 12:58:24 -0700

    notes

--- Objects/CellPhone.java
+++ Objects/CellPhone.java
@@ -13,11 +13,23 @@ public class CellPhone
     private String chargePort;
     private boolean camera;
     
-    public CellPhone(int screenW, int screenH, String color, String chargeP, boolean cam){
+    public CellPhone(int screenW, int screenH, String colour, String chargeP, boolean cam){
         screenWidth = screenW;
         screenHeight = screenH;
-        color = color;
+        color = colour;
         chargePort = chargeP;
         camera = cam;
+    }
+    
+    public void call(String name){
+        System.out.println("You call " + name);
+    }
+    
+    public void compute(){
+        System.out.println("2 + 2 = 4");
+    }
+    
+    public static void getOtherPhoneNumber(){
+        System.out.println("You get their digits");
     }
 }
