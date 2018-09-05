2ee282bf874f8a415ff5b626a7b46eeb6512b285
cheinks <cheinks1074@gmail.com>
2018-09-05 13:01:46 -0700

    notes

--- Objects/CellPhone.java
+++ Objects/CellPhone.java
@@ -47,7 +47,46 @@ public class CellPhone
     public static void getOtherPhoneNumber(){
         System.out.println("You get their digits");
     }
+    
+    /**
+     * The following is an accessor method.
+     * 
+     * @return The width of the screen.
+     */
     public int getScreenWidth(){
         return screenWidth;
+    }
+    public boolean getCamera(){
+        return camera;
+    }
+    public int getScreenHeight(){
+        return screenHeight;
+    }
+    public String getColor(){
+        return color;
+    }
+    public String getChargePort(){
+        return chargePort;
+    }
+    
+    /**
+     * The following is a mutator method.
+     * 
+     * @param The new color
+     */
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setCamera(boolean camera){
+        this.camera = camera;
+    }
+    public void setScreenWidth(int width){
+        this.screenWidth = width;
+    }
+    public void setScreenHeight(int height){
+        this.screenHeight = height;
+    }
+    public void setChargePort(String chargePort){
+        this.chargePort = chargePort;
     }
 }
--- Objects/CellphoneTester.java
+++ Objects/CellphoneTester.java
@@ -11,5 +11,19 @@ public class CellphoneTester
         CellPhone cell = new CellPhone();
         System.out.println("The screen width is: ");
         System.out.println(cell.getScreenWidth());
+        System.out.println();
+        
+        System.out.println("The old color was: ");
+        System.out.println(cell.getColor());
+        cell.setColor("Blue");
+        System.out.println("The new color is: ");
+        System.out.println(cell.getColor());
+        System.out.println();
+        
+        if(cell.getCamera()){
+            System.out.println("This phone has a camera.");
+        }else{
+            System.out.println("This phone has no camera.");
+        }
     }
 }
