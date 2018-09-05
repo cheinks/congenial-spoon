46b5e3dd5e399a45f9c4b1027fd80cd06e9c6af4
cheinks <cheinks1074@gmail.com>
2018-09-05 12:36:36 -0700

    notes

--- Objects/CellPhone.java
+++ Objects/CellPhone.java
@@ -7,12 +7,14 @@
  */
 public class CellPhone
 {
+    //Fields
     private int screenWidth;
     private int screenHeight;
     private String color;
     private String chargePort;
     private boolean camera;
     
+    //Constructors
     public CellPhone(int screenW, int screenH, String colour, String chargeP, boolean cam){
         screenWidth = screenW;
         screenHeight = screenH;
@@ -20,16 +22,32 @@ public class CellPhone
         chargePort = chargeP;
         camera = cam;
     }
+    public CellPhone(){
+        this(1920, 1080, "Black", "USB", true); //"this" refers to the constructor with that takes (int, int, String, String, boolean)
+    }
     
+    public CellPhone(int screenW, int screenH, String color, boolean cam){
+        screenWidth = screenW;
+        screenHeight = screenH;
+        this.color = color; //"this" refers to the private field at the top NOT the parameter.
+        chargePort = "USB";
+        camera = cam;
+    }
+    public CellPhone(int screenW, int screenH){
+        this(screenW, screenH, "Black", true);
+    }
+    
+    //Methods
     public void call(String name){
         System.out.println("You call " + name);
     }
-    
     public void compute(){
         System.out.println("2 + 2 = 4");
     }
-    
     public static void getOtherPhoneNumber(){
         System.out.println("You get their digits");
+    }
+    public int getScreenWidth(){
+        return screenWidth;
     }
 }
---
+++ Objects/CellphoneTester.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class fdgsdg here.
+ *
+ * @author Cody Heinks
+ * @version 9.5.18
+ */
+public class CellphoneTester
+{
+    public static void main(String[] args){
+        CellPhone cell = new CellPhone();
+        System.out.println("The screen width is: ");
+        System.out.println(cell.getScreenWidth());
+    }
+}
