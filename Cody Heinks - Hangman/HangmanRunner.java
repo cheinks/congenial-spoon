d21d69aac223527844f31a9e71cda22dc776ad61
cheinks <cheinks1074@gmail.com>
2018-10-02 12:59:27 -0700

    work

--- Cody Heinks - Hangman/HangmanRunner.java
+++ Cody Heinks - Hangman/HangmanRunner.java
@@ -20,30 +20,36 @@ public class HangmanRunner
         String restOfPhrase = guessWord;
         
         System.out.println("I'm thinking of a word...");
-        System.out.println(guessWord);
+        System.out.println("Word to guess: " + guessWord);
         for(int i = 0; i < guessLen; i++){
             System.out.print("_");
         }
         System.out.println();
         while(guesses > 0){
             System.out.println("Guess a letter:");
-            playerGuess = scanner.next();
-            if(guessWord.contains(playerGuess)){
-                int location = guessWord.indexOf(playerGuess);
+            playerGuess = scanner.nextLine();
+            int location = guessWord.indexOf(playerGuess);
+            //System.out.println("Word to guess: " + guessWord);
+            //System.out.println(location);
+            if(location >= 0){
                 while(location >= 0){
                     guessWord = restOfPhrase;
                     whatPlayerSees += guessWord.substring(0, location);
                     restOfPhrase = guessWord.substring(location + 1);
                     whatPlayerSees += "_";
                     location = restOfPhrase.indexOf(playerGuess);
+                    //System.out.println("New loc: " + location);
                 }
                 whatPlayerSees += restOfPhrase;
                 System.out.println(whatPlayerSees);
+                guessWord = whatPlayerSees;
+                restOfPhrase = guessWord;
+                whatPlayerSees = "";
             }else{
                 guesses--;
                 System.out.println("Guesses remaining: " + guesses);
             }
-            guessWord = whatPlayerSees;
+            
         }
     }
 }
--- Test/Test.java
+++ Test/Test.java
@@ -1,53 +1,59 @@
 
 /**
- * Write a description of class Test here.
+ * Write a description of class HangmanRunner here.
  *
- * @author (your name)
+ * @author Cody Heinks
  * @version (a version number or a date)
  */
 import java.util.Scanner;
 public class Test
 {
-    public static void test1(){
+    public static void playGame(){
         Scanner scanner = new Scanner(System.in);
-        String guessWord = "hello world";
+        String guessWord = "helloworld";
         System.out.println(guessWord);
         String whatPlayerSees = "";
-        String whatComputerSees = "";
+        String dashes = "";
         int guessLen = guessWord.length();
         String playerGuess = "";
         int guesses = 6;
         
+        String restOfPhrase = dashes;
+        
         System.out.println("I'm thinking of a word...");
-        System.out.println();
-        for(int i = 0; i < guessLen; i++)
-        {
-           System.out.print("_");
-           whatPlayerSees += "_";
+        System.out.println("Word to guess: " + guessWord);
+        for(int i = 0; i < guessLen; i++){
+            dashes += "_";
+            System.out.print(dashes);
         }
         System.out.println();
-        while(guesses > 0)
-        {
-            whatComputerSees = "";
+        while(guesses > 0){
             System.out.println("Guess a letter:");
-            playerGuess = scanner.next();
-            for(int i = 0; i < guessLen; i++)
-            {
-                String original = guessWord.substring(i, i + 1);
-                if(playerGuess.equals(original))
-                {
-                    whatComputerSees += playerGuess;
-                }else{
-                    whatComputerSees += "_";
+            playerGuess = scanner.nextLine();
+            int location = guessWord.indexOf(playerGuess);
+            String wordTracker = "";
+            //System.out.println("Word to guess: " + guessWord);
+            //System.out.println(location);
+            if(location >= 0){
+                while(location >= 0){
+                    dashes = restOfPhrase;
+                    whatPlayerSees += dashes.substring(0, location);
+                    restOfPhrase = dashes.substring(location + 1);
+                    whatPlayerSees += playerGuess;
+                    
+                    location = guessWord.indexOf(playerGuess);
+                    //System.out.println("New loc: " + location);
                 }
-                if(whatComputerSees.equals(whatPlayerSees))
-                {
-                    guesses--;
-                }else{
-                    whatPlayerSees = whatComputerSees;
-                }
+                whatPlayerSees += restOfPhrase;
+                System.out.println(whatPlayerSees);
+                guessWord = whatPlayerSees;
+                restOfPhrase = guessWord;
+                whatPlayerSees = "";
+            }else{
+                guesses--;
+                System.out.println("Guesses remaining: " + guesses);
             }
-            System.out.println(whatPlayerSees);
+            
         }
     }
 }
