8a1dc92d98a1f772e8523643dd90a6d785e525a9
Cody <cheinks1074@gmail.com>
2018-11-20 11:20:26 -0800

    new folder, hangman is complete

--- Cody Heinks - Hangman/HangmanRunner.java
+++ Cody Heinks - Hangman/HangmanRunner.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class HangmanRunner here.
+ * Hangman, but without any arrays.
  *
  * @author Cody Heinks
  * @version 10.09.18
@@ -10,7 +10,7 @@ public class HangmanRunner
 {
     public static void playGame(){
         Scanner scanner = new Scanner(System.in);
-        String wordToGuess = "hello world";        
+        String wordToGuess = "California";        
         int guessLen = wordToGuess.length();
         String guessWord = wordToGuess;
         String newGuessWord = guessWord;
@@ -19,6 +19,7 @@ public class HangmanRunner
         String whatPlayerSees = "";
         String playerGuess = "";
         int guesses = 6;
+        boolean win = false;
         
         System.out.println("I'm thinking of a word...");
         for(int i = 0; i < guessLen; i++){
@@ -29,8 +30,14 @@ public class HangmanRunner
         while(guesses > 0){
             System.out.println("Guess a letter:");
             playerGuess = scanner.nextLine();
+            if(playerGuess.toLowerCase().equals(wordToGuess.toLowerCase())){
+                System.out.println("Wow, you guessed it.");
+                System.out.println("My word was " + wordToGuess + ".");
+                guesses = 0;
+                win = true;
+            }
             int loc = guessWord.indexOf(playerGuess);
-            if(loc >= 0){
+            if(loc >= 0 && !win){
                 while(loc >= 0){
                     dashes = restOfPhrase;
                     whatPlayerSees += dashes.substring(0, loc);
@@ -41,14 +48,21 @@ public class HangmanRunner
                 }
                 whatPlayerSees += restOfPhrase;
                 System.out.println(whatPlayerSees);
+                if(whatPlayerSees.equals(wordToGuess)){
+                    guesses = 0;
+                    System.out.println("Congrats! You won.");
+                    System.out.println("My word was " + wordToGuess + ".");
+                }
                 dashes = whatPlayerSees;
                 restOfPhrase = dashes;
                 whatPlayerSees = "";
                 guessWord.replaceAll(playerGuess, "_");
                 newGuessWord = guessWord;
             }else{
-                guesses--;
-                System.out.println("Guesses remaining: " + guesses);
+                if(!win){
+                    guesses--;
+                    System.out.println("Guesses remaining: " + guesses);
+                }  
             }
         }
     }
