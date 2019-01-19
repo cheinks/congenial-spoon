7f7f0f0d7fcb3c74f6f3475b81e314c26e267a67
Cody <cheinks1074@gmail.com>
2019-01-18 19:23:58 -0800

    more methods

--- AP CSA Projects/Cody Heinks - ArrayList Practice/Project.java
+++ AP CSA Projects/Cody Heinks - ArrayList Practice/Project.java
@@ -53,22 +53,99 @@ public class Project{
     }
     //8 ?
     public void sort(){}
-    //9 ?
-    public static void copy(ArrayList original, ArrayList copy){
-        copy = original;
+    //9
+    public static ArrayList copy(ArrayList original){
+        ArrayList copy = new ArrayList();
+        for(Object e : original){
+            copy.add(e);
+        }
+        return copy;
     }
     //10 ?
     public void shuffle(){
-        for(String color : colors){
-            
-        }
+        for(String color : colors){}
     }
     //11
     public void reverse(){
         int pos = 0;
         String holder;
-        for(int i = colors.size()-1; i > 0; i--){
-            holder = colors.get(i);
+        for(int i = colors.size()-1; i > -1; i--){
+            holder = colors.get(colors.size()-1);
+            colors.add(pos, holder);
+            colors.remove(colors.size()-1);
+            pos++;
+        }
+    }
+    //12
+    public ArrayList extract(int start, int end){ //(inclusive, exclusive)
+        ArrayList extraction = new ArrayList();
+        for(int i = start; i < end; i++){
+            extraction.add(colors.get(i));
+        }
+        return extraction;
+    }
+    //13 ?
+    public static boolean compare(ArrayList a, ArrayList b){
+        if(a.size()>b.size()){
+            for(int i = 0; i < b.size(); i++){
+                if(!a.get(i).equals(b.get(i))){
+                    return false;
+                }
+            }
+        }else{
+            for(int i = 0; i < a.size(); i++){
+                if(!a.get(i).equals(b.get(i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    //14
+    public void swap(int a, int b){
+        String holder = colors.get(a);
+        colors.set(a, colors.get(b));
+        colors.set(b, holder);
+    }
+    //15
+    public static ArrayList join(ArrayList a, ArrayList b){
+        ArrayList c = new ArrayList();
+        for(Object e : a){
+            c.add(e);
+        }
+        for(Object e : b){
+            c.add(e);
+        }
+        return c;
+    }
+    //16
+    public static void clone(ArrayList original, ArrayList clone){
+        for(int i = 0; i < original.size(); i++){
+            clone.set(i, original.get(i));
+        }
+    }
+    //17
+    public static void empty(ArrayList list){
+        for(int i = list.size(); i > -1; i--){
+            list.remove(i);
+        }
+    }
+    //18
+    public static boolean isEmpty(ArrayList list){
+        return list.size() == 0;
+    }
+    //19 ?
+    public void trim(){}
+    //20 ?
+    public void increaseSize(){}
+    //21
+    public void replaceSecond(int e){
+        colors.set(1, colors.get(e));
+    }
+    //22
+    public void printAll(){
+        for(int i = 0; i < colors.size(); i++){
+            System.out.println(colors.get(i));
         }
     }
 }
