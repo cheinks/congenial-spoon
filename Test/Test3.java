8e07e1deae899713fee29087b0b5a5535d704e71
Cody Heinks <cheinks1074@gmail.com>
2019-01-29 12:23:21 -0800

    I figured it out!!!

--- Test/Test3.java
+++ Test/Test3.java
@@ -5,16 +5,37 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import javax.swing.*;
-import java.util.ArrayList;
+import java.lang.Math;
 public class Test3{
     public Test3(){
         JFrame frame = new JFrame();
-        
-        frame.pack();
+        frame.add(addMain());
+        frame.setSize(1000, 1000);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true); 
+    }
+    
+    public JPanel addMain(){
+        JPanel panel = new JPanel();
+        panel.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                Graphics g = panel.getGraphics();
+                g.fillRect(e.getX(), e.getY(), 10, 10);
+                g.dispose();
+            }
+        });
+        
+        panel.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                
+            }
+        });
+        
+        return panel;
     }
 }
