62a418e01770297d76b96efb130e14e7e0ed70f3
Cody Heinks <cheinks1074@gmail.com>
2019-04-29 14:27:51 -0700

    final phase complete, begin testing

---
+++ Cody Heinks - CYOAGame/WorldMap.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class WorldMap here.
+ *
+ * @author Cody Heinks
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+public class WorldMap{
+    public HashMap<String, Room>worldMap = new HashMap<String, Room>();
+}
